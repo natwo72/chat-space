7a27d96d2ee39a77a05612060c4b0b558921999f
natwo72 <natomarashi011572@gmail.com>
2020-05-22 11:45:46 +0900

    s3_5

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,30 @@
-# secrets.yml用のシンボリックリンクを追加
+
+lock '3.14.0'
+
 set :linked_files, %w{ config/secrets.yml }
 
-# 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
+set :application, 'chat-space'
 
+
+set :repo_url,  'git@github.com:natwo72/chat-space.git'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1' 
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/chatspace.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
